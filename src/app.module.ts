d8f012dd1081386fbd0582e94fa7c488732a5a4d
Stanly Calle <callestanly@gmail.com>
2026-03-26 14:09:28 -0500

    chore: scaffold app structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HealthModule } from './modules/health/health.module';
+import { CardsModule } from './modules/cards/cards.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthModule, CardsModule],
 })
 export class AppModule {}
